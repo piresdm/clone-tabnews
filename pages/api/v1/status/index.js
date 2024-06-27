a2207d1ad9cc2a06c389ea7af9c061e41e97cd47
Daniela Mendonca Pires <piresdm@msn.com>
2024-06-27 00:36:08 +0000

    add  `/api/v1/status` endpoint

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+function status(request, response) {
+  //response.status(200).send("alunos do curso.dev são pessoas acima da média");
+  //response.status(200).json({ chave: "valor" });
+  response
+    .status(200)
+    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+}
+
+export default status;
